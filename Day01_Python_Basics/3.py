4a07947a8dac5825c1cd83894b6393b69a63b2b8
Anshul Shakya <anshulshakya18168@gmail.com>
2026-01-18 19:58:22 +0530

    Day 01: Complete 3.py

---
+++ Day01_Python_Basics/3.py
@@ -0,0 +1,7 @@
+"""
+Multi line comment in Python
+This is an example of a multi-line comment using triple quotes.
+You can use this style to add detailed explanations or documentation
+"""
+
+# This is single line comment

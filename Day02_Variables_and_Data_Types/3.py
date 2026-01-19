4dd94c2d500f530ba8026d19ddc4a44ef870ed54
Anshul Shakya <anshulshakya18168@gmail.com>
2026-01-19 18:30:24 +0530

    Day 2 : 3.py

---
+++ Day02_Variables_and_Data_Types/3.py
@@ -0,0 +1,32 @@
+# Perform basics arthmetic operations
+a = 15
+b = 4
+# Addition
+sum_result = a + b
+print("Addition:", sum_result)
+# Subtraction
+sub_result = a - b
+print("Subtraction:", sub_result)
+# Multiplication
+mul_result = a * b
+print("Multiplication:", mul_result)
+# Divide
+div_result = a / b
+print("Divide:", div_result)
+# Floor Division
+floor_div_result = a // b
+print("Floor Division:", floor_div_result)
+# Modulus
+mod_result = a % b
+print("Modulus:", mod_result)
+# Exponentiation
+exp_result = a ** b
+print("Exponentiation:", exp_result)
+# Incrementing and Decrementing
+a += 1  # Increment a by 1
+print("Incremented a:", a)
+b -= 1  # Decrement b by 1
+print("Decremented b:", b)
+# Operator Precedence
+result = a + b * 2  # Multiplication has higher precedence than addition
+print("Operator Precedence Result:", result)

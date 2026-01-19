dc10f362eb83fc79202ebf93f19160a19b2c0591
Anshul Shakya <anshulshakya18168@gmail.com>
2026-01-19 18:14:42 +0530

    Add Day 02: Variables and Data Types example

---
+++ Day02_Variables_and_Data_Types/1.py
@@ -0,0 +1,26 @@
+# Integers
+num = 10
+negative_num = -5
+
+# Floats
+pi = 3.14
+gravity = 9.81
+
+# Strings
+greeting = "Hello, World!"
+name = 'Alice'
+
+# Booleans
+is_active = True
+is_admin = False
+
+# Lists
+fruits = ["apple", "banana", "cherry"]
+numbers = [1, 2, 3, 4, 5]
+
+print(fruits[0])  # Accessing the first element of the list
+
+# Tuples
+coordinates = (10.0, 20.0)
+colors = ("red", "green", "blue")
+print(coordinates[1])  # Accessing the second element of the tuple

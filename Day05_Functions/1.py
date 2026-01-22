e3e646512b1f5ede74d63c2e853bd17c4ebda4e9
Anshul Shakya <anshulshakya18168@gmail.com>
2026-01-22 19:35:13 +0530

    Add Day 05: Add two numbers using function 1.py

---
+++ Day05_Functions/1.py
@@ -0,0 +1,10 @@
+# 1. Define the function
+def add_numbers(num1, num2):
+    result = num1 + num2
+    return result
+
+# 2. Call the function
+sum_result = add_numbers(5, 10)
+
+# 3. Print the output
+print(f"The sum is: {sum_result}")

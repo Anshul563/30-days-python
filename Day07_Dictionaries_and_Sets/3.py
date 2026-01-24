054ec4f1b69b8e302e44bfb1a2dd1ac62797479e
Anshul Shakya <anshulshakya18168@gmail.com>
2026-01-24 19:49:23 +0530

    day 7 - 3.py

---
+++ Day07_Dictionaries_and_Sets/3.py
@@ -0,0 +1,7 @@
+student_profile = {"name": "Emma Watson", "student_id": 10245}
+
+# Emma got married? Let's update her name.
+student_profile["name"] = "Emma Stone"
+
+print(student_profile)
+# Output: {'name': 'Emma Stone', 'student_id': 10245}

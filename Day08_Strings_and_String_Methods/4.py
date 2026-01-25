04df175d3d81541412ed817065464b47498e1b6a
Anshul Shakya <anshulshakya18168@gmail.com>
2026-01-25 20:12:43 +0530

    feat: Add script demonstrating string `lower()` and `upper()` methods.

---
+++ Day08_Strings_and_String_Methods/4.py
@@ -0,0 +1,8 @@
+text = "PYTHON is Awesome"
+lower_text = text.lower()
+upper_text = text.upper()
+
+print(lower_text) 
+# Output: "python is awesome"
+print(upper_text)
+# Output: "PYTHON IS AWESOME"

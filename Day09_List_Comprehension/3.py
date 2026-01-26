be7262d49d393371023df01623864c22a7c221f6
Anshul Shakya <anshulshakya18168@gmail.com>
2026-01-26 19:18:59 +0530

    feat: Add script demonstrating uppercase conversion using list comprehension.

---
+++ Day09_List_Comprehension/3.py
@@ -0,0 +1,7 @@
+# A list of lowercase words
+words = ["hello", "world", "python"]
+
+# Use .upper() inside the list comprehension
+upper_words = [word.upper() for word in words]
+
+print(upper_words)

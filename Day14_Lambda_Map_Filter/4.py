f3d89870157fcf9993e65eced5281ab1ddc7d474
Anshul Shakya <anshulshakya18168@gmail.com>
2026-02-05 18:57:41 +0530

    feat: Add script demonstrating `functools.reduce` with a lambda for summing a list.

--- Day14_Lambda_Map_Filter/4.py
+++ Day14_Lambda_Map_Filter/4.py
@@ -1,9 +1,9 @@
 from functools import reduce  # We must import this first
 
-numbers = [1, 2, 3, 4]
+numbers = [1, 2, 3, 4, 5]
 
 # Use reduce to add x and y cumulatively
 total_sum = reduce(lambda x, y: x + y, numbers)
 
 print(total_sum)
-# Output: 10
+# Output: 15

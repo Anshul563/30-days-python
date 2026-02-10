64cd3e999d683c3ec828029444000a3e9d7ac842
Anshul Shakya <anshulshakya18168@gmail.com>
2026-02-10 19:40:18 +0530

    feat: Create `1.py` defining a basic class structure with a constructor and a method.

---
+++ Day16_OOP_Basics/1.py
@@ -0,0 +1,8 @@
+class ClassName:
+    def __init__(self, parameter1, parameter2):
+        self.attribute1 = parameter1
+        self.attribute2 = parameter2
+
+    def method_name(self):
+        # Action to perform
+        pass

222a0ee72180d05d99a4b886c345d686c2f34033
Anshul Shakya <anshulshakya18168@gmail.com>
2026-02-16 19:38:15 +0530

    feat: Add Player class with `__init__` method and instance attributes.

--- Day16_OOP_Basics/5.py
+++ Day16_OOP_Basics/5.py
@@ -5,7 +5,7 @@ class Player:
         self.level = level        # Creating an instance attribute
 
 # Creating the object automatically runs __init__
-player1 = Player("GamerFaker", 50)
+player1 = Player("GamerFaker", 60)
 
 print(player1.username)  # Output: GamerFaker
-print(player1.level)     # Output: 50
+print(player1.level)     # Output: 60
